1fc820c244459bcf9c0ab66db88e134849837030
CHRIS-HP\chris <chris.barr@landmarkanalytics.com>
2023-05-08 19:13:57 -0400

    Using more elements for lightbox

--- chris-barr-portfolio/script.js
+++ chris-barr-portfolio/script.js
@@ -1,48 +1,57 @@
 //-------------------------
 //Sticky section header positioning
-const header = document.querySelector('header');
-const sectionTitles = document.querySelectorAll('.project-title');
+const header = document.querySelector("header");
+const sectionTitles = document.querySelectorAll(".project-title");
 
 function calcHeaderStickyPos() {
-    const headerHeight = header.getBoundingClientRect().height;
+  const headerHeight = header.getBoundingClientRect().height;
 
-    for (let i = 0; i < sectionTitles.length; i++) {
-        const t = sectionTitles[i];
-        t.style.top = headerHeight + 'px';
-    }
+  for (let i = 0; i < sectionTitles.length; i++) {
+    const t = sectionTitles[i];
+    t.style.top = headerHeight + "px";
+  }
 }
 
 calcHeaderStickyPos();
 
-let timer = null
-window.addEventListener('resize', () =>{
-    clearTimeout(timer);
-    timer = setTimeout(calcHeaderStickyPos, 300);
+let timer = null;
+window.addEventListener("resize", () => {
+  clearTimeout(timer);
+  timer = setTimeout(calcHeaderStickyPos, 300);
 });
 
 //-------------------------
 //Lightbox Stuff
-const lightbox = document.querySelector('#lightbox');
-const thumbs = document.querySelectorAll('.thumb');
+const lbContainer = document.querySelector("#lightbox");
+const lbImgContainer = document.querySelector("#lightbox-img");
+const lbTitle = document.querySelector("#lightbox-title");
+const lbBackdrop = document.querySelector("#lightbox-backdrop");
+const thumbs = document.querySelectorAll(".thumb");
 
-lightbox.addEventListener('click', (ev) => {
-    lightbox.classList.remove('show');
-    ev.preventDefault();
-});
+function lbClose(){
+  lbContainer.classList.remove("show");
+}
 
-document.addEventListener('keydown', (ev) => {
-    if(ev.key === 'Escape'){
-        lightbox.classList.remove('show');
-    }
+//click elements to close
+lbBackdrop.addEventListener("click", lbClose);
+lbImgContainer.addEventListener("click", lbClose);
+lbTitle.addEventListener("click", lbClose);
+
+//press ESC to close
+document.addEventListener("keydown", (ev) => {
+  if (ev.key === "Escape") {
+    lbClose();
+  }
 });
 
 for (let i = 0; i < thumbs.length; i++) {
-    const t = thumbs[i];
-
-    t.addEventListener('click', (ev) => {
-        const img = t.querySelector('img');
-        lightbox.innerHTML = img.outerHTML + '<h4>'+ img.getAttribute('alt') +'</h4>'
-        lightbox.classList.add('show');
-        ev.preventDefault();
-    });
+  const t = thumbs[i];
+  t.addEventListener("click", (ev) => {
+    const img = t.querySelector("img");
+    console.log(lbImgContainer, img.src)
+    lbImgContainer.style.backgroundImage = 'url("' + img.src + '")';
+    lbTitle.innerHTML = img.getAttribute("alt");
+    lbContainer.classList.add("show");
+    ev.preventDefault();
+  });
 }
