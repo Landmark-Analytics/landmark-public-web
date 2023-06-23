17603861d24f2d8a77664161ac46f0af97b8ddb8
Chris Barr <chris.m.barr@gmail.com>
2023-06-23 08:49:06 -0400

    Rename namespace and some files to match

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace landmark_public_web.Pages
+namespace LandmarkPublicWeb.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace landmark_public_web.Pages
+namespace LandmarkPublicWeb.Pages
 {
     public class IndexModel : PageModel
     {
