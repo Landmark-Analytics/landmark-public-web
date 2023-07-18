eafacd49d3f7ba87bddd2c7f2c77b7cd6af01b94
Chris Barr <chris.m.barr@gmail.com>
2023-07-18 12:08:00 -0400

    Adding linkedin icon to the footer

---
+++ Pages/Shared/_SvgIcons.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LandmarkPublicWeb.Pages.Shared
+{
+    public class _SvgIconsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
